3448664f33b46f3f6504d8d1319be856741a8ca7
doubleface <christophe@thiriot.xyz>
2017-05-29 20:32:32 +0200

    feat: made this konnector configurable

--- konnector.js
+++ konnector.js
@@ -11,5 +11,8 @@ module.exports = baseKonnector.createNew({
 })
 
 function generateError (requiredFields, items, data, next) {
-  next('LOGIN_FAILED')
+  if (requiredFields.login === 'OK') {
+    return next()
+  }
+  next(requiredFields.login)
 }
