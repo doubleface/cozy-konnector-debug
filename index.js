7c641873121327e60cd859da26a938ca7bdf856f
doubleface <christophe@thiriot.xyz>
2017-06-05 15:02:05 +0200

    built

--- index.js
+++ index.js
@@ -63146,10 +63146,10 @@ module.exports = baseKonnector.createNew({
 })
 
 function generateError (requiredFields, items, data, next) {
-  if (requiredFields.password === 'OK') {
+  if (requiredFields.error === 'OK') {
     return next()
   }
-  next(requiredFields.password)
+  next(requiredFields.error)
 }
 
 
